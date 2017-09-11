215901ceeb6232b3e952bb701504786f95e00f31
TanguyUrvoy <tanguy.urvoy@orange.com>
2017-09-11 16:44:03 +0200

    new games, PM hierarchy seems to work

--- pmlib/pmcells.py
+++ pmlib/pmcells.py
@@ -5,7 +5,7 @@ Provides a collection of Finite Partial Monitoring algorithms for experimental s
 """
 __author__ = "Tanguy Urvoy, Pratik Gajane"
 __copyright__ = "Orange-labs 2017"
-__license__ = ""
+__license__ = "LGPL"
 __version__ = "1.0"
 __email__ = "tanguy.urvoy@orange.com, pratik.gajane@gmail.Com"
 __date__ = "2017"
@@ -24,22 +24,22 @@ import ppl
 # build the signal vector i.e. [F_{i,j}=v]_{j=1,...,M}
 # (with NxM format matrices)
 
-def signal_vec(i,v,FeedbackMatrix):
+def signal_vec(i, v, FeedbackMatrix):
     return (FeedbackMatrix[i,...] == v).astype(np.float)
 
 # build the signal matrix  i.e. [F_{i,j}=v]_{j=1,...,M}
 
-def signal_mat(i,FeedbackMatrix):
-    return np.stack([signal_vec(i,v,FeedbackMatrix) for v in set(FeedbackMatrix[i,...])])
+def signal_vecs(i, FeedbackMatrix):
+    return [signal_vec(i, v, FeedbackMatrix) for v in set(FeedbackMatrix[i,...])]
 
 
 if __name__ == "__main__":
-    pm = games.BinaryUtilityDuelingBanditPM([0.9,0.5,0.5,0.25], feedback_type = 'string')      
+    pm = games.BinaryUtilityDuelingBanditPM([0.9,0.5,0.5,0.25])      
     Fmat = pm.FeedbackMatrix
     Lmat = pm.LossMatrix
 
     print Fmat[1,:]
-    print signal_mat(1,Fmat)
+    print signal_vecs(1,Fmat)
 
     
 # Fjv: the row binary signal matrix for F_{i,j}=v (pseudo-action i/v)
@@ -51,7 +51,7 @@ def is_linear_comb(Fiv, Fdash_list):
     if len(Fdash_list) == 0:
         return True
     initial_rank = np.linalg.matrix_rank(np.vstack(Fdash_list))
-    new_rank = np.linalg.matrix_rank(np.vstack((Fdash_list,Fiv)))
+    new_rank = np.linalg.matrix_rank(np.vstack(Fdash_list + [Fiv]))
     return new_rank == initial_rank
 
 if __name__ == "__main__":
@@ -200,8 +200,12 @@ def interFacePolytope(a, b, LossMatrix):
 
 # Check if two actions are neighbours
 def areNeighbours(a, b, LossMatrix):
+    M = LossMatrix.shape[1]
     return interFacePolytope(a, b, LossMatrix).affine_dimension() >= M - 2
 
+# full signal space
+def global_signal(FeedbackMatrix):
+    return [signal_vecs(i, FeedbackMatrix) for i in range(FeedbackMatrix.shape[0])]
 
 # Returns the neigbourhood of a pair of actions
 def Neighbourhood(a, b, LossMatrix):
@@ -211,10 +215,91 @@ def Neighbourhood(a, b, LossMatrix):
     
     Nb = []
     for k in range(N):
-        if k==a or k==b or frontier <= DominationPolytope(k,LossMatrix):
+        if k==a or k==b or frontier <= DominationPolytope(k, LossMatrix):
             Nb.append(k)
     
     return Nb
+
+
+# Observability for a pair of actions
+def ObservablePair(a, b, LossMatrix, S_list):
+    Lab = LossMatrix[a,...] - LossMatrix[b,...]
+    return is_linear_comb(Lab, S_list)
+
+# Global observability for a game
+def GlobalObservableGame(pm):
+    LossMatrix, FeedbackMatrix = pm.LossMatrix, pm.FeedbackMatrix
+    N,M = pm.N, pm.M
+    assert (N,M) == LossMatrix.shape
+    assert (N,M) == FeedbackMatrix.shape
+    
+
+    global_S_list = global_signal(FeedbackMatrix)
+
+    res = True
+    why = "all pairs are globally observable."
+    for a in range(N):
+        for b in range(a+1,N):
+            res = res and ObservablePair(a, b, LossMatrix, global_S_list)
+            if not res:
+                why = "[{0},{1}] pair is not globally observable.".format(pm.Actions_dict[a],pm.Actions_dict[b])
+                return res, why
+    return res, why
+
+
+# Global observability for a game
+def LocalObservableGame(pm):
+    LossMatrix, FeedbackMatrix = pm.LossMatrix, pm.FeedbackMatrix
+    N,M = pm.N, pm.M
+    assert (N,M) == LossMatrix.shape
+    assert (N,M) == FeedbackMatrix.shape
+
+    why = "all neighbouring pairs are observable."
+    res = True
+    for a in range(N):
+        for b in range(a+1,N):
+            if areNeighbours(a, b, LossMatrix):
+                # local signal space
+                local_S_list = [signal_vecs(i, pm.FeedbackMatrix) for i in Neighbourhood(a, b, LossMatrix)]
+                
+                res = res and ObservablePair(a, b, LossMatrix, local_S_list)
+            if not res:
+                why = "[{0},{1}] pair is not locally observable.".format(pm.Actions_dict[a], pm.Actions_dict[b])
+                return res, why
+    return res, why
+
+
+def isTrivialGame(pm):
+    ndom = [isNonDominated(i, pm.LossMatrix) for i in range(pm.N)]
+    K = sum(ndom)
+    if K==1:
+        first = range(pm.N)[ndom]
+        return True, "action {0} is always the best.".format(pm.Actions_dict[first[0]])
+    else:
+        return False, "there are more than one dominant actions"
+    
+
+#
+# Return the position of this game in the partial monitoring hierarchy
+# * trivial     gives \Theta(1) minmax regret
+# * easy        gives \Theta(\sqrt(T)) minmax regret
+# * hard        gives \Theta(T^{2/3}) minmax regret
+# * intractable gives linear minmax regret
+#
+def ProblemClass(pm):
+    trivial, why = isTrivialGame(pm)
+    if trivial:
+        return "trivial", why
+    else:
+        glob, why = GlobalObservableGame(pm)
+        if glob:
+            loc, why = LocalObservableGame(pm)
+            if loc:
+                return "easy", why
+            else:
+                return "hard", why
+        else:
+            return "intractable", why
 
 
 if __name__ == "__main__":
@@ -238,7 +323,7 @@ if __name__ == "__main__":
     
 
 
-        pmg = games.BernoulliBandit([0.75,0.5,0.25], feedback_type = 'string')
+        pmg = games.BernoulliBandit([0.75,0.5,0.25])
         Lmat = pmg.LossMatrix
                                    
         print "Loss matrix:"
@@ -255,6 +340,8 @@ if __name__ == "__main__":
                 dim = pol.affine_dimension()
                 print pmg.Actions_dict[a],pmg.Actions_dict[b], dim, pol.is_empty()
 
+
+            
                 
 
 
